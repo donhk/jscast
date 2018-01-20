cbb532a9029eec9e52b7096b3ac88b95d320de4d
donhk <hk.fred@hotmail.com>
2018-01-20 15:09:07 -0600

    basic comunication with camera

--- jscast.js
+++ jscast.js
@@ -1,20 +1,40 @@
 //ES6 arrow function
 //dependencies
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const onvif = require('node-onvif');
 
-//set port to listen
-app.listen(3001, () => {
-    console.log('listening on 3001');
+// Create an OnvifDevice object
+let device = new onvif.OnvifDevice({
+    xaddr: 'http://192.168.0.13:5000/onvif/device_service',
+    user: 'admin',
+    pass: '123456'
 });
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.use(bodyParser.json());
+// Initialize the OnvifDevice object
+device.init().then((info) => {
+    // Show the detailed information of the device.
+    console.log(JSON.stringify(info, null, '  '));
 
-app.get('/', (req, res) => {
-    console.log("Hello from hell");
-    res.render('index.ejs');
+    // Get the UDP stream URL
+    let url = device.getUdpStreamUrl();
+    console.log(url);
+
+    //gather device information
+    let profile = device.getCurrentProfile();
+    console.log(JSON.stringify(profile, null, '  '));
+
+    let params = {
+        'ProfileToken': 'IPCProfilesToken0',
+        'Protocol': 'UDP',
+    };
+
+    device.services.media.getStreamUri(params).then((result) => {
+        console.log(JSON.stringify(result['data'], null, '  '));
+    }).catch((error) => {
+        console.error(error);
+    });
+
+}).catch((error) => {
+    console.error(error);
 });
+
+
