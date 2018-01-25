6d15c4447b92549d14be491dce91e55879b16982
donhk <hk.fred@hotmail.com>
2018-01-24 21:19:42 -0600

    added basic rest onvif operations

--- node/jscast.js
+++ node/jscast.js
@@ -3,6 +3,9 @@
 //dependencies
 const onvif = require('node-onvif');
 const express = require('express');
+const bodyParser = require('body-parser');
+const uniqid = require('uniqid');
+
 const app = express();
 
 //cameras on network
@@ -29,7 +32,7 @@ app.get('/initDevices', (req, res) => {
     onvif.startProbe().then((device_info_list) => {
         console.log(device_info_list.length + ' devices will be initialized.');
         let requests = device_info_list.map((info) => {
-            processDevice(info)
+            return processDevice(info);
         });
         Promise.all(requests).then(() => {
             console.log('returning cameras found ' + cameras.length);
@@ -41,27 +44,28 @@ app.get('/initDevices', (req, res) => {
     });
 });
 
+app.get('/ptzx', (req, res) => {
+    move('fred', 0, 0, 0);
+    res.json('done');
+});
+
 function processDevice(info) {
-    // Return a new promise
-    return new Promise((resolve, reject) => {
-        console.log("processing " + info.xaddrs[0]);
-        // Create an OnvifDevice object
-        let device = new onvif.OnvifDevice({
-            xaddr: info.xaddrs[0],
-            user: 'admin',
-            pass: '123456' //TODO add dynamic user list
-        });
-        console.log("init");
-        device.init().then((devinfo) => {
-            //if all is okay, add it to the list
-            console.log("pusshing " + devinfo.Manufacturer + "-" + devinfo.SerialNumber);
-            cameras.push(devinfo);
-            console.log("cameras: " + cameras.length);
-            resolve("Stuff worked!");
-        }).catch((error) => {
-            //if we hit an error, log the error TODO send error feedback
-            console.log("error initializing " + info.name + " " + error);
-            reject(Error("It broke"));
-        });
+    let device = new onvif.OnvifDevice({
+        xaddr: info.xaddrs[0],
+        user: 'admin',
+        pass: '123456' //TODO add dynamic user list
     });
+    return device.init().then(() => {
+        console.log("init done");
+        const camObj = {name: 'fred', attr: device}; //uniqid()
+        cameras.push(camObj);
+    });
+}
+
+function move(target, x, y, z) {
+    const camObj = cameras.filter((obj) => {
+        return obj.name === target;
+    });
+    const device = camObj[0].attr; //ugly but works
+    console.log("we have the device now :)");
 }
